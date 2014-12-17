8847828b5d7994ff03c978d2ffbf312a84d334c8
harishgupta <gupta.harish@outlook.com>
2014-12-17 15:54:36 +0530

    First shot

--- app/src/main/java/databrokers/BrokerMessage.java
+++ app/src/main/java/databrokers/BrokerMessage.java
@@ -8,14 +8,18 @@ import java.util.ArrayList;
 public class BrokerMessage {
 
     private String strRwaID;
+    private String strMessages;
 
-    public BrokerMessage(String RwaID) {
+
+    public BrokerMessage(String RwaID, String Messages) {
         this.strRwaID = RwaID;
+        strMessages=Messages;
     }
 
     public static ArrayList<Message> getRwaMessages(String RwaID) {
 
         ArrayList<Message> lstMessage = new ArrayList<Message>();
+
 
         return lstMessage;
     }
